ebb9d7fb942157ca4e6b0a8ab77637a028d6d67f
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-01-14 19:19:51 +0900

    Create.

---
+++ assets/js/cairo.js
@@ -0,0 +1,24 @@
+/**
+ * Updated by Takayuki Kamiyama on 2026/01/02.
+ * Team 'Red Eyes, Black Dragon.'
+ */
+
+$(function () {
+    $('a[href^=#]').click(function () {
+        const speed = 400; // millisecond
+        let href = $(this).attr("href");
+        const target = $(href === "#" || href === "" ? 'html' : href);
+        $($.browser.safari ? 'body' : 'html').animate({
+            scrollTop: position
+        }, speed, 'swing');
+        let body = 'body';
+        const userAgent = window.navigator.userAgent.toLowerCase();
+        if (userAgent.indexOf('msie') > -1 || userAgent.indexOf('trident') > -1 || userAgent.indexOf("firefox") > -1) {
+            body = 'html';
+        }
+        $(body).animate({
+            scrollTop: position
+        }, speed, 'swing');
+        return false;
+    });
+});
