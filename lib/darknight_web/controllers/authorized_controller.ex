be0f71b36815166b8288841138b854e31a93f534
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-01-17 08:06:33 +0900

    Update.

--- lib/darknight_web/controllers/authorized_controller.ex
+++ lib/darknight_web/controllers/authorized_controller.ex
@@ -1,6 +1,6 @@
 defmodule DarknightWeb.Plug.EnsureAuthorized do
   @moduledoc """
-  エンドポイント毎のアクセス認可を行うPlug
+  Plug that authorizes access for each endpoint
   """
 
   def init(default), do: default
@@ -19,7 +19,20 @@ defmodule DarknightWeb.Plug.EnsureAuthorized do
        end
   end
 
-  defp authorized?(user, controller, action) do
-    # Check authorization
+  @spec all_endpoints() :: list(%{controller: String.t(), action: String.t()})
+  def all_endpoints() do
+    endpoints_from_router =
+      DarknightWeb.Router.__routes__()
+      |> Enum.map(fn route ->
+        %{controller: "#{route.plug}", action: route.plug_opts |> Atom.to_string()}
+      end)
+
+    endpoints_from_router
+    |> Enum.uniq_by(&(&1[:controller] <> "." <> &1[:action]))
+  end
+
+  defp authorized?(conn, controller, action) do
+    controller = conn |> Phoenix.Controller.controller_module() |> to_string()
+    action = conn |> Phoenix.Controller.action_name() |> Atom.to_string()
   end
 end
